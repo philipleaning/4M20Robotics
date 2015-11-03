0a74f78e97ac227564d496e85e5652905390b9b0
philipleaning <philipleaning@gmx.com>
2015-11-03 16:19:57 +0000

    Added sheepdog class, just placeholder atm

---
+++ sheepdog.m
@@ -0,0 +1,14 @@
+classdef sheepdog < handle
+    %SHEEPDOG Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        position = zeros(1,2)
+        velocity = zeros(1,2)
+    end
+    
+    methods
+    end
+    
+end
+
