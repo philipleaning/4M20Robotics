e03174d81a5e7aebd68afc59d419ea4d6d90f602
mattffc <mattffc@hotmail.com>
2015-11-05 17:33:37 +0000

    Added NN for 4 zone based tracking of boid
    Modified sheepMass so that it's 1/distance, before it was proportional
    to the distance.

--- Dog.m
+++ Dog.m
@@ -6,11 +6,11 @@ classdef Dog < handle
         velocity = zeros(1,2)
         sheepMass = zeros(1,4)% number of sheep in each quadrant
         deltaVelocity = zeros(1,2)
-        sheepMassHistory = zeros(2000,4) %history of sheepMass over a sim
-        deltaVelocityHistory = zeros(2000,2) %history of delta velocity over a sim
-        positionHistory = zeros(2000,2)
-        sheepPosHistory = zeros(2000,4)
-        velocityHistory = zeros(2000,2)
+        sheepMassHistory = zeros(20000,4) %history of sheepMass over a sim
+        deltaVelocityHistory = zeros(20000,2) %history of delta velocity over a sim
+        positionHistory = zeros(20000,2)
+        sheepPosHistory = zeros(20000,20)
+        velocityHistory = zeros(20000,2)
     end
     
     methods
