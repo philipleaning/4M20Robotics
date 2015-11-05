c07eaa8f0d46ebceae337e4c54bf15ac5fdb07e8
mattffc <mattffc@hotmail.com>
2015-11-05 13:27:53 +0000

    Single boid following with NN working
    Messy code needs cleaning but currently NN with 10 wide 1 hidden layer
    is able to follow 1 boid well.

--- Dog.m
+++ Dog.m
@@ -6,8 +6,11 @@ classdef Dog < handle
         velocity = zeros(1,2)
         sheepMass = zeros(1,4)% number of sheep in each quadrant
         deltaVelocity = zeros(1,2)
-        sheepMassHistory = zeros(100,4) %history of sheepMass over a sim
-        deltaVelocityHistory = zeros(100,2) %history of delta velocity over a sim
+        sheepMassHistory = zeros(2000,4) %history of sheepMass over a sim
+        deltaVelocityHistory = zeros(2000,2) %history of delta velocity over a sim
+        positionHistory = zeros(2000,2)
+        sheepPosHistory = zeros(2000,4)
+        velocityHistory = zeros(2000,2)
     end
     
     methods
