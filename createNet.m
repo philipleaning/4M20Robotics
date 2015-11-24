a938e8c26fbcfb43e499d4ed0c575b430cf6cf96
philipleaning <philipleaning@gmx.com>
2015-11-24 14:45:54 +0000

    Added better createNet function, more options

--- createNet.m
+++ createNet.m
@@ -9,15 +9,36 @@ output = [];
 input = [input inputDataForNet'];
 output = [output outputDataForNet'];
 
-%% Create and Train Net
-% Create net
-net = feedforwardnet(10);
-% Train net using LM
-[net,tr] = trainlm(net,input,output);
+x = input
+t = output;
 
-%% Test & Save Net
-% Test
-y = net(inputDataForNet');
-perf = perform(net,y,outputDataForNet')
+%% Net Creation and Training
+% Choose a Training Function
+% For a list of all training functions type: help nntrain
+% 'trainlm' is usually fastest.
+% 'trainbr' takes longer but may be better for challenging problems.
+% 'trainscg' uses less memory. Suitable in low memory situations.
+trainFcn = 'trainlm';  % Levenberg-Marquardt backpropagation.
+
+% Create a Fitting Network
+hiddenLayerSize = 10;
+net = fitnet(hiddenLayerSize,trainFcn);
+
+% Setup Division of Data for Training, Validation, Testing
+net.divideParam.trainRatio = 70/100;
+net.divideParam.valRatio = 15/100;
+net.divideParam.testRatio = 15/100;
+
+% Train the Network
+[net,tr] = train(net,x,t);
+
+% Test the Network
+y = net(x);
+e = gsubtract(t,y);
+performance = perform(net,t,y)
+
+% View the Network
+%view(net);
+
 % Save in '.mat' file
 save('TrainedNet', 'net');
